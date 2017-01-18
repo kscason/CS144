cfeb646011a57a9b5665760a1a4d93a61109542b
cs144 <ksc.cason@gmail.com>
2017-01-17 21:03:19 -0800

    Part B MySQL

--- actors.sql
+++ actors.sql
@@ -1,4 +1,6 @@
-
+/*Step 1:
+	Create a table with the following schema
+	Actors(Name:VARCHAR(40), Movie:VARCHAR(80), Year:INTEGER, Role:VARCHAR(40))*/
 CREATE TABLE Actor (
     Name VARCHAR(40),
     Movie VARCHAR(80),
@@ -6,5 +8,16 @@ CREATE TABLE Actor (
     Role VARCHAR(40)
 );
 
+/*Step 2:
+	Load the actors.csv file into the Actors table.*/
 LOAD DATA LOCAL INFILE "~/data/actors.csv" INTO TABLE Actor
 FIELDS TERMINATED BY ',' OPTIONALLY ENCLOSED BY '"';
+
+/*Step 3:
+	Give me the names of all the actors in the movie 'Die Another Day'.*/
+SELECT Name FROM Actor 
+WHERE Movie='Die Another Day';
+
+/*Step 4:
+	Drop the Actors table from MySQL.*/
+DROP TABLE Actor;
