5a1caf546a0b1f07e1373317092766205d5b22b3
alex m waz <alexmwaz@g.ucla.edu>
2017-02-03 02:06:12 -0800

    the shortest yeah boi ever

--- project2/runLoad.sh
+++ project2/runLoad.sh
@@ -8,11 +8,20 @@ mysql CS144 < drop.sql
 mysql CS144 < create.sql
 
 # Compile and run the parser to generate the appropriate load files
-ant run
-#ant run-all
+#ant run
+ant run-all
 
 # If the Java code does not handle duplicate removal, do this now
 #sort ... maybe
+awk '!a[$0]++' items.csv > temp
+cp temp items.csv
+awk '!a[$0]++' users.csv > temp
+cp temp users.csv
+awk '!a[$0]++' bids.csv > temp
+cp temp bids.csv
+awk '!a[$0]++' categories.csv > temp
+cp temp categories.csv
+rm temp
 
 # Run the load.sql batch file to load the data
 mysql CS144 < load.sql
