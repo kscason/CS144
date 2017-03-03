1f3b1de096e3b54ee1dbb17e9fd4f079b233f5ea
Kaitlyn Cason <kaitlyncason@ucla.edu>
2017-03-02 21:35:47 -0800

    B2.1

--- project4/src/edu/ucla/cs/cs144/ProxyServlet.java
+++ project4/src/edu/ucla/cs/cs144/ProxyServlet.java
@@ -1,5 +1,10 @@
 package edu.ucla.cs.cs144;
 
+import java.io.BufferedReader;
+import java.net.URL;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+
 import java.io.IOException;
 import java.net.HttpURLConnection;
 import javax.servlet.Servlet;
@@ -15,5 +20,42 @@ public class ProxyServlet extends HttpServlet implements Servlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
     {
         // your codes here
+        BufferedReader br = null;
+
+        //Grab query, it there is none, don't do a suggest (obviously)
+       	String query = request.getParameter("q");
+       	if(query == null)
+       		return;
+
+       	//Create URL and open the connection
+       	String toURL = "http://google.com/complete/search?output=toolbar&q=" + query;
+        URL url = new URL(toURL);
+
+       	HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+
+       	//Get response XML data and save it
+       	br = new BufferedReader(new InputStreamReader(urlConnection.getInputStream()));
+        	
+       	String line;
+       	StringBuilder sb = new StringBuilder();
+       	while((line = br.readLine()) != null){
+       		sb.append(line);
+       	}
+       	String xml = sb.toString();
+
+       	//Print out XML data to client
+       	response.setContentType("text/xml");
+       	PrintWriter pw = response.getWriter();
+       	pw.println(xml);
+
+       	//Close and disconnect
+       	try {
+           	if (br != null) 
+           		br.close();
+       	} catch (IOException e) {
+           	e.printStackTrace();
+       	}
+        
+        urlConnection.disconnect();
     }
 }
